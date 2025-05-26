d4dc92f5cb871e274f98f5bd0cb1b9b5b2a4921c
thalyson123ui <69772569+thalyson123ui@users.noreply.github.com>
2025-05-26 12:55:01 -0300

    atualização

---
+++ script.js
@@ -0,0 +1,26 @@
+use strict';
+
+const elemToggleFunc = function (elem) { elem.classList.toggle('active'); }
+
+const navbar = document.querySelector('[data-navbar]');
+const navOpenBtn = document.querySelector('[data-nav-open-btn]');
+const navCloseBtn = document.querySelector('[data-nav-close-btn]');
+const overlay = document.querySelector('[data-overlay]');
+
+const navElemArr = [navOpenBtn, navCloseBtn, overlay];
+
+for(let i = 0; i < navElemArr.length; i++) {
+    navElemArr[i].addEventListener('click', function(){
+        elemToggleFunc(navbar);
+        elemToggleFunc(overlay);
+        elemToggleFunc(document.body);
+    })
+}
+
+/* Go Top */
+
+const goTopBtn = document.querySelector('[data-go-top]');
+
+window.addEventListener('scroll', function(){ 
+   if(window.scrollY >= 800) { goTopBtn.classList.add('active'); } else { goTopBtn.classList.remove('active'); } 
+})
